57ae9f382bed40e139618c66eec2bbaf6b5798cb
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2024-12-05 19:20:57 +0100

    05-12

--- trabajofinal/css/scripts/scripts.js
+++ trabajofinal/css/scripts/scripts.js
@@ -4,3 +4,29 @@ const menu = document.querySelector('.menu');
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+document.addEventListener('click', (e) => {
+    if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
+        menu.classList.remove('active'); // Cierra el menú si el clic es fuera
+    }
+});
+
+// script.js
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Mostrar el botón cuando el usuario haga scroll hacia abajo
+window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "block"; // Muestra el botón
+    } else {
+        scrollToTopBtn.style.display = "none"; // Oculta el botón cuando está arriba
+    }
+};
+
+// Hacer scroll hacia arriba cuando el usuario haga clic en el botón
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Desplazamiento suave hacia arriba
+    });
+});
